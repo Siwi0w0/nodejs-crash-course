d1bd0c232fec76c22eb9f784d004d6366dc611a9
Siwi0w0 <yufei.sun64@gmail.com>
2023-11-23 18:43:45 +0000

    add public folder

--- logger.js
+++ logger.js
@@ -1,6 +1,14 @@
 const EventEmitter = require('events');
 const uuid = require('uuid');
 
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log(`Called Listner`, data));
+
+logger.log('Hello Node.js');
+
 class Logger extends EventEmitter {
     log(msg) {
         // Call event
